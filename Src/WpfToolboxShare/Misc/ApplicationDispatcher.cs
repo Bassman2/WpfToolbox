53b97e8c12e09eb843c30bbe783e261902a37563
kersRalf Beckers <ralf.beckers@elektrobit.com>
2025-08-15 21:08:52 +0200

    Update

---
+++ Src/WpfToolboxShare/Misc/ApplicationDispatcher.cs
@@ -0,0 +1,28 @@
+﻿namespace WpfToolbox.Misc;
+
+public static class ApplicationDispatcher
+{
+    public static void Invoke(Action action)
+    {
+        if (Application.Current.Dispatcher.CheckAccess())
+        {
+            action();
+        }
+        else
+        {
+            Application.Current.Dispatcher.Invoke(action);
+        }
+    }
+
+    public static T Invoke<T>(Func<T> func)
+    {
+        if (Application.Current.Dispatcher.CheckAccess())
+        {
+            return func();
+        }
+        else
+        {
+            return Application.Current.Dispatcher.Invoke<T>(func);
+        }
+    }
+}
--- Src/WpfToolboxShare/Misc/WaitCursor.cs
+++ Src/WpfToolboxShare/Misc/WaitCursor.cs
@@ -1,28 +1,14 @@
 ﻿namespace WpfToolbox.Misc;
 
-public class WaitCursor : IDisposable
+public sealed class WaitCursor : IDisposable
 {
     private readonly Dispatcher dispatcher = Application.Current.Dispatcher;
-    private Cursor cursor = Cursors.Arrow;
 
-    public WaitCursor()
-    {
-        if (dispatcher.CheckAccess())
-        {
-            cursor = Mouse.OverrideCursor;
-            Mouse.OverrideCursor = System.Windows.Input.Cursors.Wait;
-        }
-        else
-        {
-            Application.Current.Dispatcher.Invoke(() =>
-            {
-                cursor = Mouse.OverrideCursor;
-                Mouse.OverrideCursor = System.Windows.Input.Cursors.Wait;
-            });
-        }
-    }
+    public WaitCursor() => SetOverrideCursor(Cursors.Wait);
 
-    public void Dispose()
+    public void Dispose() => SetOverrideCursor(null);
+
+    private void SetOverrideCursor(Cursor? cursor)
     {
         if (dispatcher.CheckAccess())
         {
@@ -30,7 +16,7 @@ public class WaitCursor : IDisposable
         }
         else
         {
-            Application.Current.Dispatcher.Invoke(() => Mouse.OverrideCursor = cursor);
+            dispatcher.Invoke(() => Mouse.OverrideCursor = cursor);
         }
     }
 }
