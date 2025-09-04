cfd963dda022ca3ac213ef6c38ef5c3c449b3619
kersRalf Beckers <ralf.beckers@elektrobit.com>
2025-09-04 18:10:42 +0200

    Update

---
+++ Src/WpfToolboxShare/Controls/CopyButton.cs
@@ -0,0 +1,31 @@
+﻿namespace WpfToolbox.Controls;
+
+public class CopyButton : Button
+{
+    static CopyButton()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(CopyButton), new FrameworkPropertyMetadata(typeof(CopyButton)));
+    }
+
+    public static readonly DependencyProperty CopyValueProperty =
+       DependencyProperty.Register("CopyValue", typeof(string), typeof(CopyButton),
+           new FrameworkPropertyMetadata(null,
+               new PropertyChangedCallback((d, e) => ((CopyButton)d).UpdateCopyValue((string?)e.NewValue))));
+
+    public string? CopyValue
+    {
+        get => (string?)GetValue(CopyValueProperty);
+        set => SetValue(CopyValueProperty, value);
+    }
+
+    private void UpdateCopyValue(string? value)
+    {
+        IsEnabled = !string.IsNullOrWhiteSpace(value);
+    }
+
+    protected override void OnClick()
+    {
+        base.OnClick();
+        Clipboard.SetText(CopyValue);
+    }
+}
