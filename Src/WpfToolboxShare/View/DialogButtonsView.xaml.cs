bab58ab6f36cbbe1f10e4b5b3b47851c5f4744f1
kersRalf Beckers <ralf.beckers@elektrobit.com>
2025-09-06 12:05:58 +0200

    Update

---
+++ Src/WpfToolboxShare/View/DialogButtonsView.xaml.cs
@@ -0,0 +1,12 @@
+namespace WpfToolbox.View;
+
+/// <summary>
+/// Interaction logic for DialogButtonsView.xaml
+/// </summary>
+public partial class DialogButtonsView : UserControl
+{
+    public DialogButtonsView()
+    {
+        InitializeComponent();
+    }
+}
