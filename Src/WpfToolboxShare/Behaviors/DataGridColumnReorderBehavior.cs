02c7d1a0d485ef03e35d872db27094dd6a3ce2e5
kersRalf Beckers <ralf.beckers@elektrobit.com>
2025-09-11 20:52:05 +0200

    Update

---
+++ Src/WpfToolboxShare/Behaviors/DataGridColumnReorderBehavior.cs
@@ -0,0 +1,38 @@
+﻿namespace WpfToolbox.Behaviors;
+
+/// <summary>
+/// Provides a behavior for <see cref="DataGrid"/> that enables column reordering
+/// and persists the column order using application settings.
+/// </summary>
+public class DataGridColumnReorderBehavior : StoreBehavior<DataGrid>
+{
+    /// <inheritdoc/>
+    protected override void OnAttached()
+    {
+        AssociatedObject.Initialized += OnInitialized;  
+        AssociatedObject.Columns.CollectionChanged += OnColumnsCollectionChanged;
+    }
+
+    /// <inheritdoc/>
+    protected override void OnDetaching()
+    {
+        AssociatedObject.Initialized -= OnInitialized;
+        AssociatedObject.Columns.CollectionChanged -= OnColumnsCollectionChanged;
+    }
+
+    
+
+    private void OnInitialized(object? sender, EventArgs e)
+    {
+        AssociatedObject.CanUserReorderColumns = true;
+
+       string s = GetSettingsValue<string>("");
+
+
+    }
+
+    private void OnColumnsCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        
+    }
+}
