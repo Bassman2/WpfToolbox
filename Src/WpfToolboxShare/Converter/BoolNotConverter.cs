978762dba1de74e888c3f1d16fb850d23331c89a
kersRalf Beckers <ralf.beckers@elektrobit.com>
2025-09-15 13:30:57 +0200

    Update

---
+++ Src/WpfToolboxShare/Converter/BoolNotConverter.cs
@@ -0,0 +1,15 @@
+﻿namespace WpfToolbox.Converter;
+
+[ValueConversion(typeof(bool), typeof(bool))]
+internal class BoolNotConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value is bool b && !b;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
