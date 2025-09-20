197d720f2bf61b68f4efda88709287792a9a3160
kersRalf Beckers <ralf.beckers@elektrobit.com>
2025-09-20 15:50:20 +0200

    Update

---
+++ Src/WpfToolboxShare/Validations/LdapPathValidationAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+using ValidationResult = System.ComponentModel.DataAnnotations.ValidationResult;
+
+namespace WpfToolbox.Validations;
+
+public partial class LdapPathValidationAttribute : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        return value is string str && !string.IsNullOrWhiteSpace(str) && LdapPathRegex().IsMatch(str)
+            ? ValidationResult.Success! : new ValidationResult($"The field {validationContext.MemberName} must be a valid LDAP path.");
+    }
+
+    [GeneratedRegex("^(?:(CN|OU|DC|O|C)=[^,]+)(?:,(?:(CN|OU|DC|O|C)=[^,]+))*$", RegexOptions.IgnoreCase)]
+    private static partial Regex LdapPathRegex();
+}
---
+++ Src/WpfToolboxShare/Validations/LdpaFilterValidationAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+using ValidationResult = System.ComponentModel.DataAnnotations.ValidationResult;
+
+namespace WpfToolbox.Validations;
+
+public partial class LdpaFilterValidationAttribute : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        return value is string str && !string.IsNullOrWhiteSpace(str) && LdapFilterRegex().IsMatch(str)
+            ? ValidationResult.Success! : new ValidationResult($"The field {validationContext.MemberName} must be a valid LDAP filter.");
+    }
+
+    private const string reg1 = @"^\([&|!](\(.*\))*\)$";
+
+    [GeneratedRegex(reg1, RegexOptions.IgnorePatternWhitespace | RegexOptions.Singleline)]
+    private static partial Regex LdapFilterRegex();
+}
