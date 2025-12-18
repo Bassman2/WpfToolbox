1290b177213022278e857ca579f82488f2d45721
Ralf Beckers <ralf.beckers@elektrobit.com>
2025-12-18 13:37:20 +0100

    Update

--- Src/WpfToolboxShare/Converter/EnumerableToEnumerationStringConverter.cs
+++ Src/WpfToolboxShare/Converter/EnumerableToEnumerationStringConverter.cs
@@ -1,11 +1,12 @@
 ﻿namespace WpfToolbox.Converter;
 
 [ValueConversion(typeof(IEnumerable), typeof(string))]
-public class EnumerableToStringConverter : IValueConverter
+public class EnumerableToEnumerationStringConverter : IValueConverter
 {
     public object? Convert(object? value, Type targetType, object parameter, CultureInfo culture)
-        => value is IEnumerable items ? string.Join(", ", items.Cast<object>().Select(g => g.ToString())) : null;
+        => value is IEnumerable items ? string.Join(Environment.NewLine, items.Cast<object>().Select(g => $"* {g.ToString()}")) : null;
 
     public object? ConvertBack(object? value, Type targetType, object parameter, CultureInfo culture)
-        =>  throw new NotImplementedException();
+        => throw new NotImplementedException();
 }
+
