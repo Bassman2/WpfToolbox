bf9c4e0ca5eaa082d7717b8ace08661fa1ab85ed
Ralf Beckers <ralf.beckers@elektrobit.com>
2026-02-17 18:27:05 +0100

    Update

---
+++ Src/WpfToolboxShare/Behaviors/ListBoxScrollIntoViewBehavior.cs
@@ -0,0 +1,22 @@
+﻿namespace WpfToolbox.Behaviors;
+
+public class ListBoxScrollIntoViewBehavior : Behavior<ListBox>
+{
+    protected override void OnAttached()
+        => AssociatedObject.SelectionChanged += OnSelectionChanged;
+
+    protected override void OnDetaching()
+        => AssociatedObject.SelectionChanged -= OnSelectionChanged;
+
+    void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (sender is ListBox listBox && listBox.SelectedItem is not null)
+        {
+            listBox.Dispatcher.BeginInvoke(() =>
+            {
+                listBox.UpdateLayout();
+                listBox.ScrollIntoView(listBox.SelectedItem);
+            });
+        }
+    }
+}
